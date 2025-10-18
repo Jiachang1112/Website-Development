687054023fbd132e3908485a90630135fd429d50
Jiachang1112 <bruce9811123@gmail.com>
2025-10-18 12:58:48 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
 export const ADMIN_EMAILS = ["bruce9811123@gmail.com"];
-export const SUPABASE_URL = "";
+export const SUPABASE_URL = "https://lfchasuywrdlkgppshxt.supabase.co/functions/v1/ocrspace-receipt";
 export const SUPABASE_ANON_KEY = "";
 export const OCR_DEFAULT_LANG = "eng";
 export const OCR_LANGS = ["eng","chi_sim","chi_tra"];
